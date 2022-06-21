ab2644cedb5327cb4609a6f1348fd8ff12d5101d
afolabi mabunmi <brownboy3047@gmail.com>
2022-06-21 19:31:39 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const fullName = 'Mabunmi Afolabi';
+
+const height = '5ft7in';
+
+const country = 'Nigeria';
+
+alert(
+    `i am ${fullName}. 
+ i am ${height} tall. 
+ i am from ${country}.
+`);
